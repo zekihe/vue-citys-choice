6b20ebab75fd062bae55b7a0ae0ef1e7bbf270bb
zekihe <zekihe@163.com>
2019-11-01 22:01:55 +0800

    update

--- src/common/commonCity.vue
+++ src/common/commonCity.vue
@@ -110,7 +110,7 @@
                 this.currentCity = item;
                 this.close();
             },
-            search (e) {
+            search (e) { 
                 this.searchData = [];
                 this.cityData.cityList.forEach(item => {
                     item.cities.forEach(cell => {
