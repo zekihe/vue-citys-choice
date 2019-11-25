43b31f1bc83acbd1f547b660be19b4030a64fd96
zekihe <zekihe@163.com>
2019-11-25 21:13:32 +0800

    updata

--- src/common/commonCity.vue
+++ src/common/commonCity.vue
@@ -110,7 +110,8 @@
                 this.currentCity = item;
                 this.close();
             },
-            search (e) {   
+            search (e) {     
+                console.log(e)
                 this.searchData = [];
                 this.cityData.cityList.forEach(item => {
                     item.cities.forEach(cell => {
